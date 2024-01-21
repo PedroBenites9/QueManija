fbd16caccfbfd50c4d7df741284ff0a031a57056
Pedro <pedro.benites99@hotmail.com>
2024-01-21 00:24:03 -0300

    Agregando Card

---
+++ src/Components/body/CardProduct.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../../css/App.css";
+
+const CardProduct = ({ remeras, gorras }) => {
+  console.log(remeras);
+
+  return (
+    <>
+      <div className="card">
+        <img src="" alt="" srcset="" />
+      </div>
+    </>
+  );
+};
+
+export default CardProduct;
--- src/Components/body/ropa.js
+++ src/Components/body/ropa.js
@@ -1,35 +1,78 @@
-import hombreRemeraBlanca from '../../img/imgHombre.JPG'
-// import hombreRemeraBlanca2 from '../../img/imgHombre.JPG'
-// import hombreRemeraBlanca3 from '../../img/imgHombre.JPG'
-// import hombreRemeraBlanca4 from '../../img/imgHombre.JPG'
-// import hombreRemeraBlanca5 from '../../img/imgHombre.JPG'
+import hombreRemeraBlanco from '../../img/Remeras/hombre/HombreRemeraBlanco.JPG'
+import hombreRemeraVerde from '../../img/Remeras/hombre/HombreRemeraVerde.JPG'
+import hombreRemeraBordo from '../../img/Remeras/hombre/HombreRemeraBordo.JPG'
+import hombreRemeraNegro from '../../img/Remeras/hombre/HombreRemeraNegro.JPG'
+import hombreRemeraCeleste from '../../img/Remeras/hombre/HombreRemeraCeleste.JPG'
+import Gorra1 from '../../img/Gorras/gorra1.jpg'
+import Gorra2 from '../../img/Gorras/gorra2.jpg'
+import Gorra3 from '../../img/Gorras/gorra3.jpg'
+import Gorra4 from '../../img/Gorras/gorra4.jpg'
 
-// agregar mas img para los productos
+const indumentaria = [
+    {
+        nameProducto: 'Remera Oversize',
+        img: { hombreRemeraVerde },
+        size: ['sm', 'md', 'lg', 'xlg'],
+        price: 8000,
+        color: 'Verde'
+    },
+    {
+        nameProducto: 'Remera Oversize',
+        img: hombreRemeraBlanco,
+        size: ['sm', 'md', 'lg', 'xlg'],
+        price: 8000,
+        color: 'Blanco'
+    },
+    {
+        nameProducto: 'Remera Oversize',
+        img: hombreRemeraNegro,
+        size: ['sm', 'md', 'lg', 'xlg'],
+        price: 8000,
+        color: 'Negro'
+    },
+    {
+        nameProducto: 'Remera Oversize',
+        img: hombreRemeraCeleste,
+        size: ['sm', 'md', 'lg', 'xlg'],
+        price: 8000,
+        color: 'Celeste'
+    },
+    {
+        nameProducto: 'Remera Oversize',
+        img: hombreRemeraBordo,
+        size: ['sm', 'md', 'lg', 'xlg'],
+        price: 8000,
+        color: 'Bordo'
+    }
+    ,
+    {
+        nameProducto: 'Gorra',
+        img: Gorra1,
+        price: 6000,
+        color: 'verde'
+    },
+    {
+        nameProducto: 'Gorra',
+        img: Gorra2,
+        price: 6000,
+        color: 'negro'
+    },
+    {
+        nameProducto: 'Gorra',
+        img: Gorra3,
+        price: 6000,
+    }
+    ,
+    {
+        nameProducto: 'Gorra',
+        img: Gorra4,
+        price: 6000,
+    }
 
-const indumentaria = [{
-    nameProducto: 'Remera Oversize',
-    img: { remera1: hombreRemeraBlanca },
-    size: ['sm', 'md', 'lg', 'xlg'],
-    price: 6000,
-    color: 'negro'
-}, {
-    nameProducto: 'Remera Oversize',
-    img: { remera1: hombreRemeraBlanca },
-    size: ['sm', 'md', 'lg', 'xlg'],
-    price: 6000,
-    color: 'marron'
-}, {
-    nameProducto: 'Remera Oversize',
-    img: { remera1: hombreRemeraBlanca },
-    size: ['sm', 'md', 'lg', 'xlg'],
-    price: 6000,
-    color: 'blanco'
-}, {
-    nameProducto: 'Remera Oversize',
-    img: { remera1: hombreRemeraBlanca },
-    size: ['sm', 'md', 'lg', 'xlg'],
-    price: 6000,
-    color: 'verde'
-},]
+
+]
 
 export default indumentaria;
+
+
+
