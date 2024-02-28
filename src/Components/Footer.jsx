e8c536e36427f135012ffdc00c5812daee845aa2
Pedro <pedro.benites99@hotmail.com>
2024-02-27 22:21:31 -0300

    estelizado footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 
+import "./../css/App.css";
+// import logo
+import logo from "./../img/logo.png";
+// import icons
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+
 const Footer = () => {
   return (
     <footer>
-      <div>el footer</div>
+      <img src={logo} alt="logo footer" />
+      <div className="footer__linksRedes">
+        <FacebookIcon className="redesSociales" />
+        <InstagramIcon className="redesSociales" />
+        <WhatsAppIcon className="redesSociales" />
+      </div>
     </footer>
   );
 };
