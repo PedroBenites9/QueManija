ba5bf489eb6c995fad88c8eacc00f9d74e1a977a
Pedro <pedro.benites99@hotmail.com>
2024-02-28 13:36:21 -0300

    agregando links en el Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,13 +9,34 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 
 const Footer = () => {
+  const handlePage = (text) => {
+    if (text === "instagram") {
+      window.location.href = "https://www.instagram.com/quemanija.urbana/";
+    } else if (text === "whatsapp") {
+      window.location.href =
+        "https://wa.me/5491151491715/?text=Hola%20quisiera%20comprarte%20una%20remera";
+    } else if (text === "facebook") {
+      window.location.href =
+        "https://www.facebook.com/profile.php?id=100091100208187";
+    }
+  };
+
   return (
     <footer>
       <img src={logo} alt="logo footer" />
       <div className="footer__linksRedes">
-        <FacebookIcon className="redesSociales" />
-        <InstagramIcon className="redesSociales" />
-        <WhatsAppIcon className="redesSociales" />
+        <FacebookIcon
+          onClick={() => handlePage("facebook")}
+          className="redesSociales"
+        />
+        <InstagramIcon
+          onClick={() => handlePage("instagram")}
+          className="redesSociales"
+        />
+        <WhatsAppIcon
+          onClick={() => handlePage("whatsapp")}
+          className="redesSociales"
+        />
         {/*
          *! hacer clickeable el foooter, agregar listas y sector de tarjetas
          */}
