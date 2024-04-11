15425925d0c43ec26f8d4165c3caf0f014a1fa62
Pedro <pedro.benites99@hotmail.com>
2024-04-11 17:14:47 -0300

    commit

--- src/Components/body/Card/ImagenesCard.jsx
+++ src/Components/body/Card/ImagenesCard.jsx
@@ -1,6 +1,7 @@
 export default function ImagenesCard({ url }) {
   return (
     <>
+      {/* colocar carousel en la imagenes */}
       {url.map((url) => (
         <div className="img__card">
           <img src={url} alt="remera" />
