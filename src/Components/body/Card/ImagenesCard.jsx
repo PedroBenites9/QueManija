f5f15d2577cb752e031563bc804cf6d02d12ad0b
Pedro Benites <pedro.benites99@hotmail.com>
2024-04-18 18:34:18 -0300

    commit notebook

--- src/Components/body/Card/ImagenesCard.jsx
+++ src/Components/body/Card/ImagenesCard.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import Slider from "react-slick";
-=======
 import Slider from "react-slick/lib/slider";
->>>>>>> pc
 
 export default function ImagenesCard({ url }) {
   const NextArrow = (props) => {
@@ -16,16 +12,10 @@ export default function ImagenesCard({ url }) {
 
   return (
     <>
-<<<<<<< HEAD
-      <Slider>
-        {url.map((url) => (
-          <div className="img__card">
-=======
       <Slider {...settings}>
         {/* colocar carousel en la imagenes */}
         {url.map((url, index) => (
           <div className="img__card" key={index}>
->>>>>>> pc
             <img src={url} alt="remera" />
           </div>
         ))}
