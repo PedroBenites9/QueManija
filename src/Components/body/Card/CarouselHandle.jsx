d918e9fb773503244a1c38e616dba29d86082fc8
Pedro <pedro.benites99@hotmail.com>
2024-05-01 13:24:31 -0300

    style home

---
+++ src/Components/body/Card/CarouselHandle.jsx
@@ -0,0 +1,52 @@
+import { useTheme } from "@emotion/react";
+import { Box } from "@mui/material";
+import React from "react";
+import SwipeableViews from "react-swipeable-views";
+import { autoPlay } from "react-swipeable-views-utils";
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+export function CarouselHandle({ images, altura }) {
+  console.log(altura);
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
+  const leKey = () => {
+    return images.map((img, i) => {
+      return i;
+    });
+  };
+  return (
+    <AutoPlaySwipeableViews
+      axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+      index={activeStep}
+      onChangeIndex={handleStepChange}
+      enableMouseEvents
+      key={leKey}
+    >
+      {images.map((step, index) => (
+        <div key={step.label}>
+          {Math.abs(activeStep - index) <= 2 ? (
+            <Box
+              component="img"
+              sx={{
+                height: `${altura}`,
+                display: "block",
+                aspectRatio: "1/1",
+                objectFit: "cover",
+                overflow: "hidden",
+                width: "100%",
+              }}
+              src={step.imgPath}
+              alt={step.label}
+            />
+          ) : null}
+        </div>
+      ))}
+    </AutoPlaySwipeableViews>
+  );
+}
--- src/Components/body/Home.jsx
+++ src/Components/body/Home.jsx
@@ -1,15 +1,40 @@
 import "../../css/home.css";
-import imgFondo from "../../img/fondoHome.png";
+import { CarouselHandle } from "./Card/CarouselHandle";
+import foto1 from "../../img/imgCarouselHome/HombreRemeraBlanco.JPG";
+import foto3 from "../../img/imgCarouselHome/HombreRemeraBordo3.JPG";
+import foto2 from "../../img/imgCarouselHome/imgMujer.JPG";
+const images = [
+  {
+    imgPath: `${foto1}`,
+  },
+  {
+    imgPath: `${foto2}`,
+  },
+  {
+    imgPath: `${foto3}`,
+  },
+];
 
 export function Home() {
   return (
     <div className="home">
-      <img src={imgFondo} alt="" className="fondoHome" />
-      <div className="home__text">
-        <h3>Define tu estilo urbano</h3>
-        <p>resalta ante el mundo </p>
-        <p>simple o llamativo, es lo que vos finas</p>
-        <button>que manija!</button>
+      <div className="home__carousel">
+        <CarouselHandle images={images} altura={"100vh"} />
+      </div>
+      <div className="home__contenedor">
+        <div className="home__text  ">
+          <h3>Define tu estilo urbano</h3>
+          <p>resalta ante el mundo </p>
+          <p>simple o llamativo, es lo que vos finas</p>
+        </div>
+        <button
+          className="button-54"
+          onClick={() => {
+            alert("hola xd");
+          }}
+        >
+          que manija!
+        </button>
       </div>
     </div>
   );
